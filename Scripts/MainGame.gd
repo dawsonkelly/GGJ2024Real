d0122ae46f2ecc1c2147e08293b1807bc946a03d
Tom <tommytiisler@gmail.com>
2024-01-27 13:18:15 -0500

    Tower placement

--- Scripts/MainGame.gd
+++ Scripts/MainGame.gd
@@ -11,8 +11,10 @@ var enemies_left = 0
 #tower spawning
 var towerSelect = 0
 var instanceTower
-var towers:Array[Resource]
 var building = false
+var cost
+
+var tower1 = preload("res://Towers/tower.tscn")
 
 var inventoryOn = false;
 var invOpen
@@ -22,23 +24,36 @@ func _ready():
 	invOpen = get_node("Inventory/Open")
 	invClosed = get_node("Inventory/Closed")
 	
-	for filePath in DirAccess.get_files_at("res://Towers/"):
-		if filePath.get_extension() == "tres":  
-			towers.append( load(filePath) )
-
-
-func _on_Spawn_Tower():
-	if building == false:
-		if towerSelect == 0 and money >= 10:
-			instanceTower = towers[0]
-
+	
+	#for filePath in DirAccess.get_files_at("res://Towers/"):
+	#	if filePath.get_extension() == "tscn":  
+	#		towers.append( load(filePath) )
 
 func _on_inventory_button_pressed():
-	if inventoryOn == false:
-		invOpen.show()
-		invClosed.hide()
-		inventoryOn = true
-	else:
-		invOpen.hide()
-		invClosed.show()
-		inventoryOn = false
+	if !building:
+		if inventoryOn == false:
+			invOpen.show()
+			invClosed.hide()
+			inventoryOn = true
+		else:
+			invOpen.hide()
+			invClosed.show()
+			inventoryOn = false
+
+
+func _on_tower1_button_pressed():
+	if building == false:
+		if towerSelect == 0 and money >= 10:
+			invOpen.hide()
+			invClosed.show()
+			inventoryOn = false
+			instanceTower = tower1.instantiate()
+			cost = 10
+			add_child(instanceTower)
+
+func tower_built():
+	building = false
+	money -= cost
+
+func cancel():
+	building = false
--- Scripts/tower.gd
+++ Scripts/tower.gd
@@ -5,19 +5,33 @@ extends StaticBody2D
 var enemies = []
 var current_enemy
 
+var building = true
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$TowerShootTimer.start()
 
 
 func _physics_process(delta):
-	pass #keep track of enemies?
+	if building == false:
+		pass
+	else:
+		global_position = get_global_mouse_position()
+		if Input.is_action_just_pressed("click"):
+			building = false
+			get_parent().tower_built()
+		if Input.is_action_just_pressed("cancel"):
+			get_parent().cancel()
+			queue_free()
+			
+	#keep track of enemies?
 
 
 func _on_tower_shoot_timer_timeout():#bang!
-	if enemies != []:
-		current_enemy = enemies[0]#selects first enemy to enter radius
-		shoot()
+	if building == false:
+		if enemies != []:
+			current_enemy = enemies[0]#selects first enemy to enter radius
+			shoot()
 
 func shoot():
 	var bullet = bulletScene.instantiate()
@@ -28,10 +42,12 @@ func shoot():
 func _on_shoot_radius_area_entered(area):
 	print("entered!")
 	if area.is_in_group("Enemy"):
-		print("enemy!")
-		enemies.append(area)
+		if building == false:
+			print("enemy!")
+			enemies.append(area)
 
 
 func _on_shoot_radius_area_exited(area):
 	if area.is_in_group("Enemy"):
-		enemies.erase(area)
+		if building == false:
+			enemies.erase(area)
