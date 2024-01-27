b26057504bd0145bad0ddd1ae14a4dae5c51de3b
Dawson Kelly <601dtkelly@gmail.com>
2024-01-27 11:33:55 -0500

    Tower changes

--- Scripts/bullet.gd
+++ Scripts/bullet.gd
@@ -1,17 +1,20 @@
 extends CharacterBody2D
 
 var bulletSpeed = 450
+var target
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$BulletDespawnTimer.start()
+	$Sprite2D.look_at(target.global_position)
+	velocity = (target.global_position - global_position).normalized()
+	#$BulletDespawnTimer.start()
 
 
 func _physics_process(delta):
-	var collision = move_and_collide((velocity + Vector2(1,0)) * bulletSpeed * delta)
+	var collision = move_and_collide((velocity.normalized()) * bulletSpeed * delta)
 	if collision:
 		#print("I collided with ", collision.get_collider().name)
-		if collision.get_collider().has_method("hit"):
+		if collision.get_collider().is_in_group("Enemy"):#if enemy hit and die
 			collision.getcollider().hit()
 			queue_free()
 
--- Scripts/tower.gd
+++ Scripts/tower.gd
@@ -2,23 +2,35 @@ extends StaticBody2D
 
 @export var bulletScene : PackedScene
 
+var enemies = []
+var current_enemy
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$TowerShootTimer.start()
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _physics_process(delta):
+	pass #keep track of enemies?
 
 
 func _on_tower_shoot_timer_timeout():#bang!
-	shoot()
+	if enemies != []:
+		current_enemy = enemies[0]#selects first enemy to enter radius
+		shoot()
 
 func shoot():
-	var bullet = bulletScene.instantiate()#spawn bullet that eventually deletes itself
-	
-	#add code here that rotates bullet
-	add_child(bullet)
-	
-	
+	var bullet = bulletScene.instantiate()
+	bullet.target = current_enemy#give bullet enemy
+	add_child(bullet)#spawn bullet that eventually deletes itself
+
+
+func _on_shoot_radius_area_entered(area):
+	print("entered!")
+	if area.is_in_group("Enemy"):
+		enemies.append(area)
+
+
+func _on_shoot_radius_area_exited(area):
+	if area.is_in_group("Enemy"):
+		enemies.erase(area)
