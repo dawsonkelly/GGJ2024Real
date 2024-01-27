d0c02d77961c7a924865146c0d232a79e94bb4d8
Tom <tommytiisler@gmail.com>
2024-01-27 11:28:29 -0500

    inventory

--- Scripts/MainGame.gd
+++ Scripts/MainGame.gd
@@ -14,7 +14,14 @@ var instanceTower
 var towers:Array[Resource]
 var building = false
 
+var inventoryOn = false;
+var invOpen
+var invClosed
+
 func _ready():
+	invOpen = get_node("Inventory/Open")
+	invClosed = get_node("Inventory/Closed")
+	
 	for filePath in DirAccess.get_files_at("res://Towers/"):
 		if filePath.get_extension() == "tres":  
 			towers.append( load(filePath) )
@@ -24,3 +31,14 @@ func _on_Spawn_Tower():
 	if building == false:
 		if towerSelect == 0 and money >= 10:
 			instanceTower = towers[0]
+
+
+func _on_inventory_button_pressed():
+	if inventoryOn == false:
+		invOpen.show()
+		invClosed.hide()
+		inventoryOn = true
+	else:
+		invOpen.hide()
+		invClosed.show()
+		inventoryOn = false
