a90e5dc830f6d9034ad8f7f2442e1c1bd8b82454
Dawson Kelly <601dtkelly@gmail.com>
2024-01-26 19:40:09 -0500

    enemies now spawn on timer

--- enemy_spawn_point.gd
+++ enemy_spawn_point.gd
@@ -1,12 +1,18 @@
 extends Node2D
 
-var spawntime = 3
+var spawnTime = 3
+var enemyPath = preload("res://enemy.tscn")
 
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$EnemySpawnTimer.start()
 
-func _ready():#spawns enemies every spawntime seconds
-	await get_tree().create_timer(3).timeout
-	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _on_enemy_spawn_timer_timeout():#spawn enemy
+	var enemy = enemyPath.instantiate()
+	add_child(enemy)
