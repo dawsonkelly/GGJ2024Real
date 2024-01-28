eaa515d7b19a3800d7f822fbe7561f604c1afcc6
Dawson Kelly <601dtkelly@gmail.com>
2024-01-28 14:31:57 -0500

    adpush

--- Scripts/ad_script.gd
+++ Scripts/ad_script.gd
@@ -2,6 +2,5 @@ extends Node2D
 
 
 
-
 func _on_close_button_pressed():
 	$AdImage.hide()
--- ad_handler.gd
+++ ad_handler.gd
@@ -1,12 +1,22 @@
 extends Node2D
 
-@export var ads : Array[PackedScene]
+@export var ads : Array[Node2D]
+var r = RandomNumberGenerator.new()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$AdTimer.start()
+	for i in (len(ads)-1):
+		ads[i].hide()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _on_timer_timeout():#pop up every few seconds
+	r.randomize()
+	var num = r.randi_range(0, (len(ads)-1))
+	print(num)
+	ads[num].show()
